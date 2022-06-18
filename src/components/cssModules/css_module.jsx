e8f9eb707ea62dc4e81639594068498d49477e4c
Nazmul <csenazmul96@gmail.com>
2022-06-18 18:12:30 +0600

    css module

--- src/components/cssModules/css_module.jsx
+++ src/components/cssModules/css_module.jsx
@@ -1,6 +1,35 @@
+import Card from "../UI/Card";
+import style from './css_module.module.css'
+import React, {useState} from "react";
 const CssModule = ()=>{
+    const [error, setError] = useState(false)
+    const [title, setTitle] = useState('')
+
+
+    const titleHandle = event => {
+        event.preventDefault()
+        setTitle(event.target.value)
+        setError(false)
+    }
+    const addTopic = e =>{
+        e.preventDefault()
+        if(title.trim().length === 0){
+            setError(true)
+            return
+        }
+    }
     return (
-        <div>sdf</div>
+        <Card className="expense">
+            <p>This is Module CSS component</p>
+            <div className={style.mulule_css}>
+                <div className={`${style['form-control']} ${error && style.invalid}`}>
+                    <input type="text" onChange={titleHandle} />
+                </div>
+                <div className="new-expense__actions">
+                    <button  type="submit" onClick={addTopic} >Add New</button>
+                </div>
+            </div>
+        </Card>
     )
 }
 export default CssModule
